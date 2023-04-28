43cf238cef441f93e070167266459f78f0f0fb1c
Ihor <ihorfront88@gmail.com>
2023-04-28 14:19:02 +0300

    refactor: del api

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/users", usersRoutes);
+app.use("/users", usersRoutes);
 app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
